3857c938558a68c2a3cf9174e6c6a6ef74077689
nikhil-kumar255 <67291996+nikhil-kumar255@users.noreply.github.com>
2020-07-24 13:48:14 -0400

    Update generate_ntuple.C
    made some minor changes. added a parameter length which accepts the proper size of the file.

--- generate_ntuple.C
+++ generate_ntuple.C
@@ -1,8 +1,9 @@
 void generate_ntuple(){
   TNtuple* ntuple = new TNtuple("ntuple", "data from bob azmoun", "position:intensity");
-  ntuple->ReadFile("test.csv");
-  TH1F* hIntensity = new TH1F("hIntensity", "Intensity vs position, I think?;intensity;position", 540, 0, 539);
+  ntuple->ReadFile("Values_Cleaved_Sanded.csv");//Values_Cleaved_Sanded,Values_Cleaved_Not_Sanded
+  int length = 785;// 539 for sculpted tip. 785 for cleaved and sanded tip, 790 for cleaved not sanded
+  TH1F* hIntensity = new TH1F("hIntensity", "Intensity vs Position;Position;Intensity", length +1, 0, length);
   ntuple->Draw("position>>hIntensity", "intensity");
-  ntuple->SaveAs("ntuple.root");
+  ntuple->SaveAs("Values_Cleaved_Sanded.root");
   return;
 }
