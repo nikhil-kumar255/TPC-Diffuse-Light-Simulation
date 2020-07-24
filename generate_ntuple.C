35f2cdb551979248237c6440316aa482968911a9
nikhil-kumar255 <67291996+nikhil-kumar255@users.noreply.github.com>
2020-07-24 14:07:33 -0400

    Update generate_ntuple.C
    added hist option to remove error bars

--- generate_ntuple.C
+++ generate_ntuple.C
@@ -3,7 +3,7 @@ void generate_ntuple(){
   ntuple->ReadFile("Values_Cleaved_Sanded.csv");//Values_Cleaved_Sanded,Values_Cleaved_Not_Sanded
   int length = 785;// 539 for sculpted tip. 785 for cleaved and sanded tip, 790 for cleaved not sanded
   TH1F* hIntensity = new TH1F("hIntensity", "Intensity vs Position;Position;Intensity", length +1, 0, length);
-  ntuple->Draw("position>>hIntensity", "intensity");
+  ntuple->Draw("position>>hIntensity", "intensity","hist");
   ntuple->SaveAs("Values_Cleaved_Sanded.root");
   return;
 }
